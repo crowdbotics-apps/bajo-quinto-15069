8308d1b9ec216e98b8d4ceff1d3bf4b3ee0ee475
Crowdbotics <team@crowdbotics.com>
2020-03-25 19:01:56 +0000

    Initial commit.

---
+++ backend/bajo_quinto_15069/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for bajo_quinto_15069 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bajo_quinto_15069.settings')
+
+application = get_wsgi_application()
